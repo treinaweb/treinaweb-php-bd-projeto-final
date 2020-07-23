d08676c8a8df0e1b73dcd326db5fb07d7fbbdfa3
Elton Fonseca <elton869@gmail.com>
2020-07-23 16:05:15 -0300

    27 - Criando a conexao com banco de dados

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require_once "autoload/autoload.php";
 
 use App\Repositorio\Curso;
+use App\Uteis\Conexao;
 
 $configBancoDeDados = require 'config/banco-de-dados.php';
+$conexao = new Conexao($configBancoDeDados);
 
-var_dump($configBancoDeDados);
+var_dump($conexao);
 
 $cursoRepositorio = new Curso;
 
