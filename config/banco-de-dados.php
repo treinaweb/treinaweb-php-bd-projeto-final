6bfedd3c0b2a869cf2f6c1e86fb6a5ddab349710
Elton Fonseca <elton869@gmail.com>
2020-07-23 15:36:11 -0300

    26 - Criando arquivo de configurações do banco de dados

---
+++ config/banco-de-dados.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'servidor' => 'localhost',
+    'banco-de-dados' => 'treinaweb',
+    'usuario' => 'root',
+    'senha' => '123456789'
+];
--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require_once "autoload/autoload.php";
 
 use App\Repositorio\Curso;
 
+$configBancoDeDados = require 'config/banco-de-dados.php';
+
+var_dump($configBancoDeDados);
+
 $cursoRepositorio = new Curso;
 
 require_once "exibicao/principal.php";
